9f6f9681403c8e75e7d285d0ec235ee4b6a0d4e6
orlando1080 <lewislsewell@gmail.com>
2025-06-22 19:28:38 +0100

    feat: building a walking skeleton part 1 - API
    Build the database with Dbcontext, create entities and add UserController

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController(DataContext dataContext) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<AppUser>>> GetUsers() => await dataContext.Users.ToListAsync();
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        AppUser? user = await dataContext.Users.FindAsync(id);
+
+        return  user is null ? NotFound() : user;
+    }
+}
